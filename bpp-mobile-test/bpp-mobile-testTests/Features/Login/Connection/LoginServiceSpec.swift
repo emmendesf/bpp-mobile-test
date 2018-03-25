80f2b1ed8ffb16669b2633aae1dc53d93f5e0820
Emerson Mendes Filho <>
2018-03-25 17:53:18 -0300

    Configure initial structure for tests

---
+++ bpp-mobile-test/bpp-mobile-testTests/Features/Login/Connection/LoginServiceSpec.swift
@@ -0,0 +1,9 @@
+//
+//  LoginServiceSpec.swift
+//  bpp-mobile-testTests
+//
+//  Created by Emerson Mendes Filho on 25/03/2018.
+//  Copyright © 2018 Emerson Mendes Filho. All rights reserved.
+//
+
+import Foundation
