0c7a63cf9ca9119a0b0d6329cc0f13cc19d9a4bc
Emerson Mendes Filho <>
2018-03-25 12:43:35 -0300

    Create project

---
+++ bpp-mobile-test/bpp-mobile-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bpp-mobile-test
+//
+//  Created by Emerson Mendes Filho on 25/03/2018.
+//  Copyright © 2018 Emerson Mendes Filho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ bpp-mobile-test/bpp-mobile-testTests/bpp_mobile_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  bpp_mobile_testTests.swift
+//  bpp-mobile-testTests
+//
+//  Created by Emerson Mendes Filho on 25/03/2018.
+//  Copyright © 2018 Emerson Mendes Filho. All rights reserved.
+//
+
+import XCTest
+@testable import bpp_mobile_test
+
+class bpp_mobile_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
