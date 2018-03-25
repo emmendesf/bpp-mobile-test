8a684a4a4834447d1fc454399d7e8d720d048a9c
Emerson Mendes Filho <>
2018-03-25 18:06:48 -0300

    Create ViewConfiguration protocol Add Snapkit to project

---
+++ bpp-mobile-test/bpp-mobile-test/Shared/View/ViewConfiguration.swift
@@ -0,0 +1,23 @@
+//
+//  ViewConfiguration.swift
+//  bpp-mobile-test
+//
+//  Created by Emerson Mendes Filho on 25/03/2018.
+//  Copyright © 2018 Emerson Mendes Filho. All rights reserved.
+//
+
+import Foundation
+
+protocol ViewConfiguration {
+    func buildViewHierarchy()
+    func setupConstraints()
+    func configureViews()
+}
+
+extension ViewConfiguration {
+    func setupViews() {
+        buildViewHierarchy()
+        setupConstraints()
+        configureViews()
+    }
+}
