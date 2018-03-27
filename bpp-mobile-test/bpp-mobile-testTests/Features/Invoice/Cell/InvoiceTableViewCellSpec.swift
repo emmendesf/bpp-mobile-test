21c66f2879dd7f642e25205701b247667ed7fa36
Emerson Mendes Filho <>
2018-03-27 00:20:02 -0300

    Tests for InvoiceTableViewCell Add missing pods

---
+++ bpp-mobile-test/bpp-mobile-testTests/Features/Invoice/Cell/InvoiceTableViewCellSpec.swift
@@ -0,0 +1,77 @@
+//
+//  InvoiceTableViewCellSpec.swift
+//  bpp-mobile-testTests
+//
+//  Created by Emerson Mendes Filho on 26/03/2018.
+//  Copyright © 2018 Emerson Mendes Filho. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Nimble_Snapshots
+
+@testable import bpp_mobile_test
+
+class InvoiceTableViewCellSpec: QuickSpec {
+    override func spec() {
+        var sut: InvoiceTableViewCell!
+        var invoiceMock: Invoice!
+        
+        describe("given InvoiceTableViewCell") {
+            beforeEach {
+                let urlPath = Bundle(for: InvoiceTableViewCellSpec.self).url(forResource: "invoice_object", withExtension: "json")!
+                let data = try! Data(contentsOf: urlPath, options: .mappedIfSafe)
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .formatted(DateFormatter.transactionFormattedDateFormatter)
+                invoiceMock = try! decoder.decode(Invoice.self, from: data)
+                
+                sut = InvoiceTableViewCell()
+                sut.frame = CGRect(x: 0, y: 0, width: 320, height: 162)
+                sut.backgroundColor = .white
+            }
+            context("when creating a new cell with settled transaction status") {
+                beforeEach {
+                    invoiceMock.transactionStatus = .settled
+                    sut.setup(invoiceMock)
+                }
+                
+                it ("then it should show the correct layout") {
+                    expect(sut) == snapshot()
+                }
+            }
+            
+            context("when creating a new cell with pending transaction status") {
+                beforeEach {
+                    invoiceMock.transactionStatus = .pending
+                    sut.setup(invoiceMock)
+                }
+                
+                it ("then it should show the correct layout") {
+                    expect(sut) == snapshot()
+                }
+            }
+            
+            context("when creating a new cell with reversed transaction status") {
+                beforeEach {
+                    invoiceMock.transactionStatus = .reversed
+                    sut.setup(invoiceMock)
+                }
+                
+                it ("then it should show the correct layout") {
+                    expect(sut) == snapshot()
+                }
+            }
+            
+            context("when creating a new cell with declined transaction status") {
+                beforeEach {
+                    invoiceMock.transactionStatus = .declined
+                    sut.setup(invoiceMock)
+                }
+                
+                it ("then it should show the correct layout") {
+                    expect(sut) == snapshot()
+                }
+            }
+        }
+    }
+}
